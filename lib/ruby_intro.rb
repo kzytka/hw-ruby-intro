178e3a3f627107af18232e87cf3622707e21c5d7
Kiley Zytka <kiley.zytka@gmail.com>
2022-03-03 06:07:06 +0000

    Updated ruby_intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,60 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return "0"
+  else
+    @total = 0
+    for i in arr
+      @total += i
+    end
+    return "#@total"
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return "0"
+  elsif arr.length == 1
+    return arr[0]
+  else
+    @largest1 = 0
+    @largest2 = 0
+    for i in arr
+      if i > @largest1
+        @largest1 = i
+      end
+    end
+    for j in arr
+      if j > @largest2 && j < @largest1
+        @largest2 = j
+      end
+    end
+    @total = @largest1 + @largest2
+    return "#@total"
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    return true
+  elsif arr.any? {n} || n.zero?
+    return false
+  else
+    arr.combination(2).any? {|a, b| a + b == n}
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str1 = "Hello"
+  return str1 + " " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  @s = s.upcase
+  s.scan(/\A[^AEIOU]\/[A-Z])
 end
 
 def binary_multiple_of_4? s
